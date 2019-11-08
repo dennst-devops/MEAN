53b74d03933511b7d2595d44749be2368cc64fc8
Dennis S <dennst@gmail.com>
2019-11-07 16:22:15 -0800

    Restful tasks continued

--- Angular/RestfulTaskApi/public/src/app/app.component.ts
+++ Angular/RestfulTaskApi/public/src/app/app.component.ts
@@ -8,5 +8,18 @@ import { HttpService } from './http.service';
 })
 export class AppComponent {
   title = 'app';
-  constructor(private _httpService: HttpService){}
+  content;
+  constructor(private _httpService: HttpService) { }
+
+  onButtonClick(): void {
+    console.log(`Click event is working`);
+    // our http response is an Observable, store it in a variable
+    let tempObservable = this._httpService.getTasks();
+    // subscribe to the Observable and provide the code we would like to do with our data from the response
+    tempObservable.subscribe(data => {
+      console.log("Got all our tasks!", data)
+      this.content = data;
+    });
+
+  }
 }
--- Angular/RestfulTaskApi/public/src/app/http.service.ts
+++ Angular/RestfulTaskApi/public/src/app/http.service.ts
@@ -11,10 +11,7 @@ export class HttpService {
   }
 
   getTasks() {
-    // our http response is an Observable, store it in a variable
-    let tempObservable = this._http.get('/tasks');
-    // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got all our tasks!", data));
+    return this._http.get('/tasks');
   }
   byId() {
     // our http response is an Observable, store it in a variable
